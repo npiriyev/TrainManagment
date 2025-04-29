02b39d4641f56b01f4c9d87db5c6677efeb99c55
npiriyev <npiriyev@gmail.com>
2025-04-29 15:43:23 +0400

    updated post method

--- TrainManagment/Controllers/ComponentManagmentController.cs
+++ TrainManagment/Controllers/ComponentManagmentController.cs
@@ -23,40 +23,53 @@ public class ComponentManagmentController : ControllerBase
     [AllowAnonymous]
     public async Task<IActionResult> CreateComponentAsync([FromBody] CreateComponentModel item)
     {
-       var dbItem = await _repository.GetAsync(x=>x.UniqueNumber == item.UniqueNumber);
-       if (dbItem == null)
-       {
-           var newEntity = new TrainComponent()
-           {
-               Id = item.Id,
-               UniqueNumber = item.UniqueNumber,
-               Name = item.Name,
-               CanAssign = item.CanAssign
-           };
-           await _repository.AddAsync(newEntity);
-       }
-       else
-       {
-           dbItem.CanAssign = item.CanAssign;
-           dbItem.Name = item.Name;
-           dbItem.CanAssign = item.CanAssign;
-           dbItem.UniqueNumber = item.UniqueNumber;
-           await _repository.UpdateAsync(dbItem);
-       }
+        TrainComponent resultEntity;
+        var dbItem = await _repository.GetAsync(x => x.UniqueNumber == item.UniqueNumber);
+    
+        if (dbItem == null)
+        {
+            // Create new entity
+            var newEntity = new TrainComponent()
+            {
+                Id = item.Id,
+                UniqueNumber = item.UniqueNumber,
+                Name = item.Name,
+                CanAssign = item.CanAssign
+            };
+            await _repository.AddAsync(newEntity);
+            resultEntity = newEntity;
+        }
+        else
+        {
+            // Update existing entity
+            dbItem.CanAssign = item.CanAssign;
+            dbItem.Name = item.Name;
+            dbItem.UniqueNumber = item.UniqueNumber;
+            await _repository.UpdateAsync(dbItem);
+            resultEntity = dbItem;
+        }
 
-       await _repository.SaveChangesAsync();
-       return Ok();
+        await _repository.SaveChangesAsync();
+    
+        // Return the created/updated entity
+        return Ok(new {
+            id = resultEntity.Id,
+            uniqueNumber = resultEntity.UniqueNumber,
+            name = resultEntity.Name,
+            canAssign = resultEntity.CanAssign
+        });
     }
     
+    
     [HttpGet]
-    [Authorize]
+    [AllowAnonymous]
     public async Task<IEnumerable<TrainComponent>> GetAllAsync()
     {
        return await _repository.GetAllAsync();
     }
     
     [HttpPost]
-    [Authorize]
+    [AllowAnonymous]
     public async Task<TrainComponent> UpdateComponentAsync([FromBody] UpdateComponentModel item)
     {
         var dbItem = await _repository.GetAsync(x => x.UniqueNumber == item.UniqueNumber);
